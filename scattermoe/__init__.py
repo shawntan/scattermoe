7a317de3e3c0068624cded71d9065a7cf3625f7a
Shawn Tan <shawn@wtf.sg>
2024-10-07 04:07:49 +0000

    Works.

--- scattermoe/__init__.py
+++ scattermoe/__init__.py
@@ -1,5 +1,5 @@
 from . import kernels
 from . import parallel_experts
 from . import mlp
-from triton_implementation import padded_block_indices
-from parallel_experts import ParallelExperts
+from .triton_implementation import padded_block_indices
+from .parallel_experts import ParallelExperts
